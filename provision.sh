d3bc5895b6adadc9145b8ba356e57124fb62eafc
Dariusz Luksza <dariusz.luksza@gmail.com>
2024-08-07 22:42:43 +0100

    Run Homebrew installer in noninteractive mode

--- provision.sh
+++ provision.sh
@@ -17,7 +17,7 @@ fi
 
 if ! [ $(command -v brew >/dev/null 2>&1) ]; then
     printf 'Installing HomeBrew ...'
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     eval "$(/opt/homebrew/bin/brew shellenv)"
     echo ' DONE'
 fi
