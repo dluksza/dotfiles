db6480daed0904f2076dbbca09d85a4f3a34cc0f
Dariusz Luksza <dariusz.luksza@gmail.com>
2025-04-06 21:58:04 +0100

    Update Hammerspoon configuration

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -2,15 +2,15 @@ hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
 
 hs.hotkey.bind({ "cmd", "alt" }, "T", function()
-  hs.application.launchOrFocus("Kitty")
+	hs.application.launchOrFocus("WezTerm")
 end)
 
-hs.hotkey.bind({ "cmd", "alt" }, "E", function()
-  hs.application.launchOrFocus("Emacs")
-end)
+-- hs.hotkey.bind({ "cmd", "alt" }, "E", function()
+-- 	hs.application.launchOrFocus("Emacs")
+-- end)
 
 hs.hotkey.bind({ "cmd", "alt" }, "B", function()
-  hs.application.launchOrFocus("Brave Browser")
+	hs.application.launchOrFocus("Brave Browser")
 end)
 
 -- function swichAudioOutput()
@@ -21,14 +21,18 @@ end)
 id = nil
 modal = hs.hotkey.modal.new({ "cmd", "alt" }, "O")
 function modal:entered()
-  local allOutDevides = hs.audiodevice.allOutputDevices()
-  id = hs.alert.show("TEST", hs.alert.defaultStyle, hs.screen.mainScreen(), "infinity")
+	local allOutDevides = hs.audiodevice.allOutputDevices()
+	id = hs.alert.show("TEST", hs.alert.defaultStyle, hs.screen.mainScreen(), "infinity")
 end
 
 function modal:exited()
-  hs.alert.closeSpecific(id)
+	hs.alert.closeSpecific(id)
 end
 
-modal:bind("", "0", hs.audiodevice.defaultOutputDevice(), function() modal:exit() end)
+modal:bind("", "0", hs.audiodevice.defaultOutputDevice(), function()
+	modal:exit()
+end)
 -- modal:bind("", "escape", function() modal:exit() end)
-modal:bind("", "escape", function() modal:exit() end)
+modal:bind("", "escape", function()
+	modal:exit()
+end)
