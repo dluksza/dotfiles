821cecb0561c7a14b28bd9b467c71e1887c725e1
Darek Luksza <dariusz.luksza@gmail.com>
2025-10-07 21:59:56 +0100

    Update wezterm configuration
    Add blur to the console background

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -11,6 +11,7 @@ config.font = wezterm.font("Fira Code", { weight = 450 })
 -- config.show_close_tab_button_in_tabs = false
 config.show_new_tab_button_in_tab_bar = false
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.macos_window_background_blur = 10
 
 config.keys = {
 	{ key = "Enter", mods = "SHIFT", action = wezterm.action({ SendString = "\x1b\r" }) },
